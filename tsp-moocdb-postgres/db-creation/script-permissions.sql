fd8200a6cc1f9401b84b055d23e99efc2571e6cd
Alexis Mousset <contact@amousset.eu>
2014-11-16 21:54:37 +0100

    First commit

--- tsp-moocdb-postgres/db-creation/script-permissions.sql
+++
@@ -1,8 +0,0 @@
-/* Grant read access to pg_reader */
-GRANT USAGE ON SCHEMA public to pg_reader;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO pg_reader;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO pg_reader;
-
-/* Grant write access to pg_writer */
-GRANT USAGE ON SCHEMA public to pg_writer;
-GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO pg_writer;
